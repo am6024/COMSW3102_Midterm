f0c9a6cd93e19df5ae443589a88c47f76cc32f27
am6024 <am6024@columbia.edu>
2024-03-29 08:43:33 -0400

    Phase 3 complete ready for submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "./notes";
+import Area from "./Area";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((note) => (
+      <Area onAdd={addNote} />
+      {notes.map((note, index) => (
         <Note
-          key={note.key}
+          key={index}
+          id={index}
           title={note.title}
           content={note.content}
+          onDelete={deleteNote}
         />
       ))}
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Notes(props) {
+  function Click() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <button onClick={Click}>DELETE</button>
     </div>
   );
 }
